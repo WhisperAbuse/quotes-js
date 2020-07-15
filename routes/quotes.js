98b3ec5ab10978374c0e70c380c1c49d8844964b
Whisp <incwhisper@gmail.com>
2020-07-15 21:55:00 +0300

    create quote

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const QuoteModel = require("../models/quote");
 
-router.post("/", (req, res) => {
-  res.send("Hello");
+router.post("/", async (req, res, next) => {
+  try {
+    const { author, text, ...extra } = req.body;
+
+    if (Object.keys(extra).length > 0) {
+      return res
+        .status(400)
+        .send(`bodya contains extra fields:\n ${Object.keys(extra)}`);
+    }
+
+    const quote = new QuoteModel({
+      author,
+      text,
+      user_id: req.user._id,
+    });
+
+    res.send(await quote.save());
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/tea", (req, res) => {
