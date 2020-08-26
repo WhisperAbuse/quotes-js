b600bb31ab99ba800c0b9557f92dc73226941ca2
Whisp <incwhisper@gmail.com>
2020-08-26 14:53:59 +0300

    update

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const config = require("./config");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const authMiddleware = require("./middleware/auth");
 
 const app = express();
 
 app.use(bodyParser.json());
 
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use("/quotes", require("./routes/quotes"));
 app.use("/auth", require("./routes/auth"));
+app.use("/users", require("./routes/users"));
 
 app.get("/", (req, res) => res.send("Hi bich"));
 
--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,16 @@ const router = require("express").Router();
 const QuoteModel = require("../models/quote");
 const UserModel = require("../models/user");
 
-
 // get all users
-router.get("/", (req, res, next) => {
-    const users = await UserModel.find();
-    res.send(users);
-})
+router.get("/", async (req, res, next) => {
+  const users = await UserModel.find();
+  res.send(users);
+});
 
 // get user by id
-router.get("/:id", (req, res, next) => {
-    const user = await UserModel.findById(req.params.id);
-    res.send(user);
-})
+router.get("/:id", async (req, res, next) => {
+  const user = await UserModel.findById(req.params.id);
+  res.send(user);
+});
 
-module.exports = router
+module.exports = router;
